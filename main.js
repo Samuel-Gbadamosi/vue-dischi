6208f31d6aff8cec53a93b4e16309530b30a5bdb
samuel <damosisamuel@gmail.com>
2021-02-09 15:30:32 +0100

    vuejs

---
+++ main.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+  el:'#app',
+  data:{
+    pictures :[],
+    genereAc:'all'
+  },
+  mounted(){
+    axios.
+    get('https://flynn.boolean.careers/exercises/api/array/music').then((result) =>{
+     this.pictures = result.data.response;
+     console.log(this.pictures);
+
+    })
+    .catch(error =>{
+      console.log('error');
+    })
+  },
+  methods:{
+
+    selectores(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((result) =>{
+     let filterPics = result.data.response;
+      console.log(this.filterPics);
+
+      
+    })
+
+
+  }
+  }
+});
